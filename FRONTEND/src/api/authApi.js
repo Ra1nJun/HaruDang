c920f097a2e06799ed4b0ae08d174ccbff80d307
Ra1nJun <Over4ction@gmail.com>
2026-02-17 00:22:38 +0900

    revise req header type

--- FRONTEND/src/api/authApi.js
+++ FRONTEND/src/api/authApi.js
@@ -4,9 +4,7 @@ import qs from 'qs';
 export function login(email, password) {
     const data = qs.stringify({ username: email, password });
     return httpClient.post('/auth/login', data, {
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
+    withCredentials: true
     });
 }
 
